9af0fc177bb54a8e9bd8f8e15d0dc620821b178c
Beta Ziliani <beta@mpi-sws.org>
2014-05-05 12:56:30 +0200

    reduction after unfolding of constant. it should not be necessary, but for the moment we do it just to reduce the number of steps.

--- pretyping/munify.ml
+++ pretyping/munify.ml
@@ -724,29 +724,29 @@ and try_step ?(stuck=NotStucked) dbg conv_t ts env sigma0 (c, l as t) (c', l' as
       else 
 	begin
 	  debug_str "Rigid-Delta-ConsR" dbg;
-	  unify' ~conv_t (dbg+1) ts env sigma0 t (get_def_app_stack env t')
+	  unify' ~conv_t (dbg+1) ts env sigma0 t (evar_apprec ts env sigma0 (get_def_app_stack env t'))
 	end
   | Const _, _ when has_definition ts env c && stuck = NotStucked ->
       if is_stuck ts env sigma0 t then
-	begin
-	  debug_str "Rigid-Delta-ConsL" dbg;
-	  try_step ~stuck:StuckedLeft dbg conv_t ts env sigma0 t t'
-	end
+	try_step ~stuck:StuckedLeft dbg conv_t ts env sigma0 t t'
       else
-	unify' ~conv_t (dbg+1) ts env sigma0 (get_def_app_stack env t) t'
+        begin
+          debug_str "Rigid-Delta-ConsL" dbg;
+          unify' ~conv_t (dbg+1) ts env sigma0 (evar_apprec ts env sigma0 (get_def_app_stack env t)) t'
+	end
   | _, Const _ when has_definition ts env c' && stuck = StuckedLeft ->
     debug_str "Rigid-Delta-ConsR" dbg;
-    unify' ~conv_t (dbg+1) ts env sigma0 t (get_def_app_stack env t')
+    unify' ~conv_t (dbg+1) ts env sigma0 t (evar_apprec ts env sigma0 (get_def_app_stack env t'))
   | Const _, _ when has_definition ts env c && stuck = StuckedRight ->
     debug_str "Rigid-Delta-ConsL" dbg;
-    unify' ~conv_t (dbg+1) ts env sigma0 (get_def_app_stack env t) t'
+    unify' ~conv_t (dbg+1) ts env sigma0 (evar_apprec ts env sigma0 (get_def_app_stack env t)) t'
 
   | _, Const _ when has_definition ts env c' ->
     debug_str "Rigid-Delta-ConsR" dbg;
-    unify' ~conv_t (dbg+1) ts env sigma0 t (get_def_app_stack env t')
+    unify' ~conv_t (dbg+1) ts env sigma0 t (evar_apprec ts env sigma0 (get_def_app_stack env t'))
   | Const _, _ when has_definition ts env c ->
     debug_str "Rigid-Delta-ConsL" dbg;
-    unify' ~conv_t (dbg+1) ts env sigma0 (get_def_app_stack env t) t'
+    unify' ~conv_t (dbg+1) ts env sigma0 (evar_apprec ts env sigma0 (get_def_app_stack env t)) t'
 
   (* Lam-EtaR *)
   | _, Lambda (name, t1, c1) when l' = [] ->
